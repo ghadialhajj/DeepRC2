f039d406fed14403bd73dd960b4ca86fc8ca65e1
ghadialhajj <ghadi.alhajj8@gmail.com>
2023-01-09 19:46:06 +1100

    Clean and add variables and structure for BM

--- deeprc/examples/parallel_single_task_cnn.py
+++ deeprc/examples/parallel_single_task_cnn.py
@@ -26,7 +26,7 @@ from deeprc.utils import Logger
 #
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_updates', help='Number of updates to train for. Recommended: int(1e5). Default: int(1e3)',
-                    type=int, default=int(20e3))
+                    type=int, default=int(10e3))
 # type=int, default=int(20))
 parser.add_argument('--evaluate_at', help='Evaluate model on training and validation set every `evaluate_at` updates. '
                                           'This will also check for a new best model for early stopping. '
@@ -50,32 +50,19 @@ parser.add_argument('--sample_n_sequences', help='Number of instances to reduce 
                     type=int, default=int(1e4))
 parser.add_argument('--learning_rate', help='Learning rate of DeepRC using Adam optimizer. Default: 1e-4',
                     type=float, default=1e-4)
-# parser.add_argument('--device', help='Device to use for NN computations, as passed to `torch.device()`. '
-#                                      'Default: "cuda:0".',
-#                                       type=str, default="cuda:1")
-# parser.add_argument('--rnd_seed', help='Random seed to use for PyTorch and NumPy. Results will still be '
-#                                       'non-deterministic due to multiprocessing but weight initialization will be the'
-#                                        ' same). Default: 0.',
-#                     type=int, default=0)
 parser.add_argument('--idx', help='Index of the run. Default: 0.',
                     type=int, default=1)
 
-# parser.add_argument('--ideal', help='1 (True) means "use ideal attention values, 0 (False), otherwise. Default: 0.',
-#                     type=int, default=0)
-
 args = parser.parse_args()
 # Set computation device
-device_name = "cuda:1"  # + str(int((args.ideal + args.idx)%2))
+device_name = "cuda:0"  # + str(int((args.ideal + args.idx)%2))
 device = torch.device(device_name)
 
 seeds = [92, 9241, 5149, 41, 720, 813, 48525]
-# Set random seed (will still be non-deterministic due to multiprocessing but weight initialization will be the same)
-torch.manual_seed(seeds[args.idx])
-np.random.seed(seeds[args.idx])
 
 # root_dir = "/home/ghadi/PycharmProjects/DeepRC2/deeprc"
 root_dir = "/storage/ghadia/DeepRC2/deeprc"
-dataset_type = "all_observed"
+dataset_type = "benchmarking1"
 # root_dir = "/itf-fi-ml/shared/users/ghadia/deeprc"
 # root_dir = "/fp/homes01/u01/ec-ghadia/DeepRC2/deeprc"
 # root_dir = "/cluster/work/projects/ec35/ec-ghadia/"
@@ -83,10 +70,16 @@ base_results_dir = "/results/singletask_cnn/ideal"
 
 config = {"sequence_reduction_fraction": 0.1, "reduction_mb_size": int(5e3),
           "timestamp": datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S'), "prop": 0.77,
-          "dataset": "n_600_wr_0.050%_po_100%", "pos_weight": 1, "train_then_freeze": False,
-          "staged_training": False, "plain_DeepRC": True, "Branch": "AdHoc1"}  # , "tag": ["AdHoc1.3.1"]}
+          "dataset": "n_600_wr_0.050%_po_20%", "pos_weight": 100,
+          "plain_DeepRC": False, "Branch": "AdHoc1"}  # , "tag": ["AdHoc1.3.1"]}
+
+strategies = ["TE", "TASTE", "T-SAFTE", "FG"]
 
 
+# TE: Train Everthing
+# TASTE: Train Attention and Sequence Embedding first, then Train Everything
+# T-SAFTE: Train Attention and Sequence Embedding first, then Freeze the first part and Train Everything
+# FG: Forced Guidance: attention is provided rather than learned (regarded of label trueness)
 # all_observed datasets:
 # n_600_wr_0.100%_po_100%
 # n_600_wr_0.300%_po_100%
@@ -105,7 +98,7 @@ config = {"sequence_reduction_fraction": 0.1, "reduction_mb_size": int(5e3),
 # n_600_wr_0.700%_po_80%_sw_50%
 
 
-def get_sample_reps(num_per_class: int = 2, both_sides: bool = True):
+def get_sample_reps(num_per_class: int = 1, both_sides: bool = False):
     main_path = f"{root_dir}/datasets/{dataset_type}/{config['dataset']}"
     meta_path = f"{main_path}/metadata.tsv"
     meta_csv = pd.read_csv(meta_path, sep="\t")
@@ -137,22 +130,6 @@ def log_reps(df_dict: dict):
 # Append current timestamp to results directory
 results_dir = os.path.join(f"{base_results_dir}_{config['dataset']}", config["timestamp"])
 
-if config["staged_training"]:
-    group = f"TASTE_n_up_{args.n_updates}_pw_{config['pos_weight']}"
-    if config["train_then_freeze"]:
-        group = f"TTF_n_up_{args.n_updates}_prop_{config['prop']}_pw_{config['pos_weight']}"
-else:
-    group = f"TE_n_up_{args.n_updates}_pw_{config['pos_weight']}"
-run = wandb.init(project="DeepRC_PlainW_StanData", group=group)  # , tags=config["tag"])
-run.name = f"results_idx_{str(args.idx)}"  # config["run"] +   # += f"_ideal_{config['ideal']}"
-
-df_dict = get_sample_reps(2, True)
-log_reps(df_dict)
-wandb.finish()
-exit(7)
-
-wandb.config.update(args)
-wandb.config.update(config)
 #
 # Create Task definitions
 #
@@ -185,46 +162,77 @@ trainingset, trainingset_eval, validationset_eval, testset_eval = make_dataloade
 )
 dl_dict = {"trainingset_eval": trainingset_eval, "validationset_eval": validationset_eval,
            "testset_eval": testset_eval}
-#
-# Create DeepRC Network
-#
-# Create sequence embedding network (for CNN, kernel_size and n_kernels are important hyper-parameters)
-sequence_embedding_network = SequenceEmbeddingCNN(n_input_features=20 + 3, kernel_size=args.kernel_size,
-                                                  n_kernels=args.n_kernels, n_layers=1)
-# Create attention network
-attention_network = AttentionNetwork(n_input_features=args.n_kernels, n_layers=2, n_units=32)
-# Create output network
-output_network = OutputNetwork(n_input_features=args.n_kernels,
-                               n_output_features=task_definition.get_n_output_features(), n_layers=1, n_units=32)
-# Combine networks to DeepRC network
-model = DeepRC(max_seq_len=30, sequence_embedding_network=sequence_embedding_network,
-               attention_network=attention_network,
-               output_network=output_network,
-               consider_seq_counts=False, n_input_features=20, add_positional_information=True,
-               sequence_reduction_fraction=config["sequence_reduction_fraction"],
-               reduction_mb_size=config["reduction_mb_size"], device=device).to(device=device)
-#
-# Train DeepRC model
-#
 logger = Logger(dataloaders=dl_dict)
 
-train(model, task_definition=task_definition, trainingset_dataloader=trainingset,
-      trainingset_eval_dataloader=trainingset_eval, learning_rate=args.learning_rate,
-      early_stopping_target_id='binary_target_1',  # Get model that performs best for this task
-      validationset_eval_dataloader=validationset_eval, logger=logger, n_updates=args.n_updates,
-      evaluate_at=args.evaluate_at, device=device, results_directory=f"{root_dir}{results_dir}", prop=config["prop"],
-      log_training_stats_at=args.log_training_stats_at,  # Here our results and trained models will be stored
-      train_then_freeze=config["train_then_freeze"], staged_training=config["staged_training"],
-      plain_DeepRC=config["plain_DeepRC"], log=False)
 
-# logger.log_stats(model=model, device=device, step=args.n_updates)
+for strategy in strategies:
+    if strategy == "TE":
+        group = f"TE_n_up_{args.n_updates}_pw_{config['pos_weight']}"
+        config.update({"train_then_freeze": False, "staged_training": False, "forced_attention": False})
+    elif strategy == "TASTE":
+        group = f"TASTE_n_up_{args.n_updates}_prop_{config['prop']}_pw_{config['pos_weight']}"
+        config.update({"train_then_freeze": False, "staged_training": True, "forced_attention": False})
+    elif strategy == "T-SAFTE":
+        group = f"T-SAFTE_n_up_{args.n_updates}_prop_{config['prop']}_pw_{config['pos_weight']}"
+        config.update({"train_then_freeze": True, "staged_training": True, "forced_attention": False})
+    elif strategy == "FG":
+        group = f"FG_n_up_{args.n_updates}_pw_{config['pos_weight']}"
+        config.update({"train_then_freeze": False, "staged_training": False, "forced_attention": True})
 
-#
-# Evaluate trained model on testset
-#
+    # Set random seed (will still be non-deterministic due to multiprocessing but weight init will be the same)
+    torch.manual_seed(seeds[args.idx])
+    np.random.seed(seeds[args.idx])
 
-scores, sequence_scores = evaluate(model=model, dataloader=testset_eval, task_definition=task_definition, device=device)
-wandb.run.summary.update(scores["binary_target_1"])
-wandb.run.summary.update(sequence_scores["sequence_class"])
-print(f"Test scores:\n{scores}")
-wandb.finish()
+    run = wandb.init(project="Benchmarking1", group=group)  # , tags=config["tag"])
+    run.name = f"results_idx_{str(args.idx)}"  # config["run"] +   # += f"_ideal_{config['ideal']}"
+
+    if args.idx == 0:
+        df_dict = get_sample_reps()
+        log_reps(df_dict)
+
+    wandb.config.update(args)
+    wandb.config.update(config)
+
+    #
+    # Create DeepRC Network
+    #
+    # Create sequence embedding network (for CNN, kernel_size and n_kernels are important hyper-parameters)
+    sequence_embedding_network = SequenceEmbeddingCNN(n_input_features=20 + 3, kernel_size=args.kernel_size,
+                                                      n_kernels=args.n_kernels, n_layers=1)
+    # Create attention network
+    attention_network = AttentionNetwork(n_input_features=args.n_kernels, n_layers=2, n_units=32)
+    # Create output network
+    output_network = OutputNetwork(n_input_features=args.n_kernels,
+                                   n_output_features=task_definition.get_n_output_features(), n_layers=1, n_units=32)
+    # Combine networks to DeepRC network
+    model = DeepRC(max_seq_len=30, sequence_embedding_network=sequence_embedding_network,
+                   attention_network=attention_network,
+                   output_network=output_network,
+                   consider_seq_counts=False, n_input_features=20, add_positional_information=True,
+                   sequence_reduction_fraction=config["sequence_reduction_fraction"],
+                   reduction_mb_size=config["reduction_mb_size"], device=device,
+                   forced_attention=config["forced_attention"]).to(device=device)
+    #
+    # Train DeepRC model
+    #
+
+    train(model, task_definition=task_definition, trainingset_dataloader=trainingset,
+          trainingset_eval_dataloader=trainingset_eval, learning_rate=args.learning_rate,
+          early_stopping_target_id='binary_target_1',  # Get model that performs best for this task
+          validationset_eval_dataloader=validationset_eval, logger=logger, n_updates=args.n_updates,
+          evaluate_at=args.evaluate_at, device=device, results_directory=f"{root_dir}{results_dir}", prop=config["prop"],
+          log_training_stats_at=args.log_training_stats_at,  # Here our results and trained models will be stored
+          train_then_freeze=config["train_then_freeze"], staged_training=config["staged_training"],
+          plain_DeepRC=config["plain_DeepRC"], log=False)
+
+    # logger.log_stats(model=model, device=device, step=args.n_updates)
+
+    #
+    # Evaluate trained model on testset
+    #
+
+    scores, sequence_scores = evaluate(model=model, dataloader=testset_eval, task_definition=task_definition, device=device)
+    wandb.run.summary.update(scores["binary_target_1"])
+    wandb.run.summary.update(sequence_scores["sequence_class"])
+    print(f"Test scores:\n{scores}")
+    wandb.finish()
