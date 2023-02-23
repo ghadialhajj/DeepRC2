b3419fd6ac55232faddfcb46fe7137a322010ac9
ghadialhajj <ghadi.alhajj8@gmail.com>
2023-02-23 12:20:09 +0100

    Run related changes

--- deeprc/examples/parallel_single_task_cnn.py
+++ deeprc/examples/parallel_single_task_cnn.py
@@ -55,13 +55,13 @@ parser.add_argument('--idx', help='Index of the run. Default: 0.',
 
 args = parser.parse_args()
 # Set computation device
-device_name = "cuda:0"  # + str(int((args.ideal + args.idx)%2))
+device_name = "cuda:1"  # + str(int((args.ideal + args.idx)%2))
 device = torch.device(device_name)
 
 seeds = [92, 9241, 5149, 41, 720, 813, 48525]
 
-root_dir = "/home/ghadi/PycharmProjects/DeepRC2/deeprc"
-# root_dir = "/storage/ghadia/DeepRC2/deeprc"
+# root_dir = "/home/ghadi/PycharmProjects/DeepRC2/deeprc"
+root_dir = "/storage/ghadia/DeepRC2/deeprc"
 dataset_type = "all_observed10"
 # root_dir = "/itf-fi-ml/shared/users/ghadia/deeprc"
 # root_dir = "/fp/homes01/u01/ec-ghadia/DeepRC2/deeprc"
@@ -78,6 +78,8 @@ strategies = ["PDRC"]
 datasets = ["n_600_wr_0.150%_po_100%_nmotif_10_sw_20%_po2_0%", "n_600_wr_0.150%_po_80%_nmotif_10_sw_20%_po2_20%",
             "n_600_wr_0.150%_po_60%_nmotif_10_sw_20%_po2_40%", "n_600_wr_0.150%_po_100%_nmotif_10_sw_80%_po2_0%",
             "n_600_wr_0.150%_po_80%_nmotif_10_sw_80%_po2_20%", "n_600_wr_0.150%_po_60%_nmotif_10_sw_80%_po2_40%"]
+datasets = ["n_600_wr_1.000%_po_100%"]
+
 print("defined variables")
 
 
@@ -192,7 +194,7 @@ for datastet in datasets:
         torch.manual_seed(seeds[args.idx])
         np.random.seed(seeds[args.idx])
 
-        run = wandb.init(project="SSM2", group=group, reinit=True)  # , tags=config["tag"])
+        run = wandb.init(project="Knut", group=group, reinit=True)  # , tags=config["tag"])
         run.name = f"results_idx_{str(args.idx)}"  # config["run"] +   # += f"_ideal_{config['ideal']}"
         # DeepRC_PlainW_StanData, Explore_wFPs
         if args.idx == 0:
@@ -234,7 +236,7 @@ for datastet in datasets:
               prop=config["prop"],
               log_training_stats_at=args.log_training_stats_at,  # Here our results and trained models will be stored
               train_then_freeze=config["train_then_freeze"], staged_training=config["staged_training"],
-              plain_DeepRC=config["plain_DeepRC"], log=True)
+              plain_DeepRC=config["plain_DeepRC"], log=False)
 
         # logger.log_stats(model=model, device=device, step=args.n_updates)
 
