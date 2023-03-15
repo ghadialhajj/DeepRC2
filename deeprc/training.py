15390dcb7701ae50339faf438087fd78f30bb1bf
ghadialhajj <ghadi.alhajj8@gmail.com>
2023-03-15 11:17:14 +0100

    Add early stopping

--- deeprc/training.py
+++ deeprc/training.py
@@ -8,6 +8,7 @@ Contact -- widrich@ml.jku.at
 import os
 from itertools import chain
 
+from estorch.pytorchtools import EarlyStopping
 import numpy as np
 import torch
 from tqdm import tqdm
@@ -114,6 +115,9 @@ def train(model: torch.nn.Module, task_definition: TaskDefinition, early_stoppin
          If True, missing target values will be ignored for training. This can be useful if auxiliary tasks are not
          available for all samples but might increase the computation time per update.
     """
+
+    # initialize the early_stopping object
+    early_stopping = EarlyStopping(patience=5, verbose=True)
 
     if log:
         logger.log_stats(model=model, device=device, step=0, log_and_att_hists=True)
@@ -268,6 +272,7 @@ def train(model: torch.nn.Module, task_definition: TaskDefinition, early_stoppin
                         scores, sequence_scores = evaluate(model=model, dataloader=validationset_eval_dataloader,
                                                            task_definition=task_definition, device=device)
                         scoring_loss = scores[early_stopping_target_id]['loss']
+                        early_stopping(scoring_loss, model)
 
                         print(f" ...done!")
                         tprint(f"[validation] u: {update:07d}; scores: {scores};")
@@ -293,9 +298,13 @@ def train(model: torch.nn.Module, task_definition: TaskDefinition, early_stoppin
                             # saver_loader.save_to_file(filename=f'best_so_far_u{update}.tar.gzip')
 
                     if update >= n_updates:
-                        logger.log_motifs(list(model.sequence_embedding.parameters())[0].cpu().detach().numpy(),
-                                          step=update)
                         break
+
+                if early_stopping.early_stop:
+                    print("Early stopping")
+                    logger.log_motifs(list(model.sequence_embedding.parameters())[0].cpu().detach().numpy(),
+                                      step=update)
+                    break
             update_progess_bar.close()
         finally:
             # In any case, save the current model and best model to a file
