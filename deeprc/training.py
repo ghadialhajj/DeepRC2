f3f7363af6130c4168efef8055c365c7850b453b
ghadialhajj <ghadi.alhajj8@gmail.com>
2023-09-04 10:03:19 +0200

    Disable logging motifs

--- deeprc/training.py
+++ deeprc/training.py
@@ -227,8 +227,10 @@ def train(model: torch.nn.Module, task_definition: TaskDefinition, early_stoppin
                     # Add to tensorboard
                     if update % log_training_stats_at == 0 or update == 1:
                         if log:
-                            logg_and_att_hists = True if update == 1 or update % (4 * log_training_stats_at) == 0 else False
-                            logger.log_stats(model=model, device=device, step=update, log_and_att_hists=logg_and_att_hists)
+                            logg_and_att_hists = True if update == 1 or update % (
+                                        4 * log_training_stats_at) == 0 else False
+                            logger.log_stats(model=model, device=device, step=update,
+                                             log_and_att_hists=logg_and_att_hists)
                         group = 'training/'
                         # Loop through tasks and add losses to tensorboard
                         pred_losses = task_definition.get_losses(raw_outputs=logit_outputs, targets=targets)
@@ -311,10 +313,10 @@ def train(model: torch.nn.Module, task_definition: TaskDefinition, early_stoppin
             state.update(saver_loader.load_from_ram())  # load best model so far
             saver_loader.save_to_file(filename=f'best_u{update}.tar.gzip')
             print('Finished Training!')
-            if log:
-                logger.log_stats(model=model, device=device, step=n_updates, log_and_att_hists=True, log_per_kernel=True)
-                logger.log_motifs(list(model.sequence_embedding.parameters())[0].cpu().detach().numpy(),
-                                  step=update)
+            # if log:
+            #     logger.log_stats(model=model, device=device, step=n_updates, log_and_att_hists=True, log_per_kernel=True)
+            #     logger.log_motifs(list(model.sequence_embedding.parameters())[0].cpu().detach().numpy(),
+            #                       step=update)
 
     except Exception as e:
         with open(logfile, 'a') as lf:
