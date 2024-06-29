30dbd68bb9b3f0d99d768db4f6bb163ae428078b
ghadialhajj <ghadi.alhajj8@gmail.com>
2024-06-29 19:23:58 +0200

    Files for cohorts 2+3

---
+++ deeprc/examples/t1d_logreg/ensemble.py
@@ -0,0 +1,83 @@
+import pickle as pkl
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
+
+with open('/storage/ghadia/DeepRC2/results/T1D_AC_clip/folds_outputs.pkl', 'rb') as f:
+    cohort1_results = pkl.load(f)
+    cohort1_results = {i: list(cohort1_results[i]) for i in range(5)}
+
+for i in range(5):
+    cohort1_results[i][1] = cohort1_results[i][1].cpu().numpy()
+
+with open('/storage/ghadia/DeepRC2/results/T1D_AC_clip/folds_outputs23.pkl', 'rb') as f:
+    cohorts2_3_results = pkl.load(f)
+    cohorts2_3_results = {i: list(cohorts2_3_results[i]) for i in range(5)}
+
+for i in range(5):
+    cohorts2_3_results[i][1] = cohorts2_3_results[i][1].cpu().numpy()
+
+split_inds = np.random.RandomState(seed=0).permutation(1298)
+split_inds = np.array_split(split_inds, 5)
+train_inds = np.concatenate([subset[:207] for subset in split_inds])
+val_inds = np.concatenate([subset[207:] for subset in split_inds])
+
+targets1 = cohort1_results[0][2].cpu().numpy()
+sample_ids1 = cohort1_results[0][0]
+results = {}
+
+# getting the models and the predictions
+
+raw_data = np.hstack([cohort1_results[i][1] for i in range(5)])
+training_data = raw_data[train_inds]
+val_data = raw_data[val_inds]
+
+results['avg_raw_ensemble'] = val_data.mean(1)
+results['avg_prediction_ensemble'] = (val_data > 0).mean(1)
+
+param_grid = {
+    'C': [0.01, 0.1, 1, 10, 100, 1000, 10000],
+    'penalty': ['l2', 'l1'],
+    'solver': ['liblinear']  # 'liblinear' is a good choice for small datasets and l1/l2 penalties
+}
+
+validation_fold = np.zeros(1298)
+validation_fold[val_inds] = 0  # last 20% for validation
+validation_fold[train_inds] = -1  # first 80% for training
+
+ps = PredefinedSplit(test_fold=validation_fold)
+
+raw_logreg = LogisticRegression(max_iter=1000, class_weight='balanced')
+grid_search = GridSearchCV(estimator=raw_logreg, param_grid=param_grid, cv=ps, scoring='roc_auc')
+grid_search.fit(raw_data, targets1)
+raw_logreg = grid_search.best_estimator_
+results['raw_logreg'] = raw_logreg.predict_proba(val_data)[:, 1]
+
+thresholded_logreg = LogisticRegression(max_iter=1000, class_weight='balanced')
+grid_search = GridSearchCV(estimator=thresholded_logreg, param_grid=param_grid, cv=ps, scoring='roc_auc')
+grid_search.fit(raw_data > 0, targets1)
+thresholded_logreg = grid_search.best_estimator_
+results['thresholded_logreg'] = thresholded_logreg.predict_proba(val_data > 0)[:, 1]
+
+# evaluate the models
+
+metrics = {}
+for name, predictions in results.items():
+    metrics[name] = roc_auc_score(targets1[val_inds], predictions)
+
+best_model = max(metrics, key=metrics.get)
+
+# apply best model to cohorts 2+3
+
+if True:
+    test_data = np.hstack([cohorts2_3_results[i][1] for i in range(5)])
+    # test_predictions = test_data.mean(1)
+    test_predictions = raw_logreg.predict_proba(test_data)[:, 1]
+
+    test_targets = cohorts2_3_results[0][2].cpu().numpy()
+    test_metrics = {"roc_auc": roc_auc_score(test_targets, test_predictions),
+                    "roc_pr": average_precision_score(test_targets, test_predictions)}
+
+    print(f'test AUC: {test_metrics["roc_auc"]:.3f}, test PR AUC: {test_metrics["roc_pr"]:.3f}')
---
+++ deeprc/examples/t1d_logreg/get predictions.py
@@ -0,0 +1,84 @@
+import numpy as np
+import torch
+from deeprc.task_definitions import TaskDefinition, BinaryTarget
+from deeprc.dataset_readers import make_dataloaders, no_sequence_count_scaling, make_test_dataloader
+from deeprc.architectures import DeepRC, SequenceEmbeddingCNN, AttentionNetwork, OutputNetwork
+from deeprc.training import evaluate
+import re
+import os
+import pickle
+
+device = torch.device("cuda:0")
+task_definition = TaskDefinition(
+    targets=[BinaryTarget(column_name='ML_class', true_class_value='T1D', pos_weight=1.)]).to(device=device)
+
+res_dict = {}
+best_ids = [18, 21, 21, 21, 21]
+# todo use the same split as before
+# todo split cohort 1 into tr+val, build a logreg morel based on the predictions of the 5 DeepRC models, then test it
+# on cohort 2+3
+# generate 1298 integers from 0 to 1297, shuffle them and split them into 5 folds
+split_inds = np.random.RandomState(seed=0).permutation(1298)
+split_inds = np.array_split(split_inds, 5)
+
+dl_config = {"task_definition": task_definition,
+             "metadata_file_id_column": 'filename',
+             "metadata_file_column_sep": ',',
+             "sequence_column": 'amino_acid',
+             "sequence_counts_column": 'templates',
+             "sample_n_sequences": 10000,
+             "sequence_counts_scaling_fn": no_sequence_count_scaling,
+             "with_test": False}
+
+# trainingset, trainingset_eval, *_ = make_dataloaders(
+#     metadata_file="/storage/ghadia/DeepRC2/deeprc/datasets/T1D/cohort1_wws.csv",
+#     repertoiresdata_path="/storage/ghadia/DeepRC2/deeprc/datasets/T1D/cohort_1",
+#     split_inds=split_inds,
+#     **dl_config)
+
+testset_eval = make_test_dataloader(metadata_file="/storage/ghadia/DeepRC2/deeprc/datasets/T1D/cohorts2_3.csv",
+                                    repertoiresdata_path="/storage/ghadia/DeepRC2/deeprc/datasets/T1D/cohorts2_3",
+                                    split_inds=list(range(857)),
+                                    **dl_config
+                                    )
+
+for fold in range(5):
+    # if fold != 4:
+    #     continue
+    n_kernels = 64 if fold else 32
+    sequence_embedding_network = SequenceEmbeddingCNN(n_input_features=20 + 3, kernel_size=5, n_kernels=n_kernels,
+                                                      n_layers=1)
+    attention_network = AttentionNetwork(n_input_features=n_kernels, n_layers=2, n_units=32)
+    output_network = OutputNetwork(n_input_features=n_kernels,
+                                   n_output_features=task_definition.get_n_output_features(), n_layers=1, n_units=64)
+    model = DeepRC(max_seq_len=30, sequence_embedding_network=sequence_embedding_network,
+                   attention_network=attention_network, output_network=output_network, consider_seq_counts=False,
+                   n_input_features=20, add_positional_information=True, sequence_reduction_fraction=0.1,
+                   reduction_mb_size=int(5e4), device=device).to(device=device)
+
+    pattern = r'Validation loss: (\d+\.\d+)'
+
+    best_idx = best_ids[fold]
+    main_dir = f"/storage/ghadia/DeepRC2/results/T1D_AC_clip"
+    # filename = f"{main_dir}/log_f{fold}.txt"
+    # with open(filename, 'r') as file:
+    #     # read a list of lines into data
+    #     data = file.readlines()
+    #     best_idx = min([(float(re.search(pattern, line)[1]), idx) for idx, line in enumerate(data) if
+    #                     "Validation loss" in line])[1]
+
+    model_dir = f"/storage/ghadia/DeepRC2/results/T1D_AC_clip/fold{fold}/idx{best_idx}/checkpoint/"
+    file = [file_name for file_name in os.listdir(model_dir) if file_name.endswith('.tar.gzip')][0]
+    checkpoint = torch.load(model_dir + file)
+    model.load_state_dict(checkpoint['model'])
+
+    tr_sample_ids, tr_raw_outputs, tr_targets = evaluate(model=model, dataloader=testset_eval,
+                                                         task_definition=task_definition, device=device)
+
+    res_dict[fold] = (tr_sample_ids, tr_raw_outputs, tr_targets)
+
+with open(f'{main_dir}/folds_outputs23.pkl', 'wb') as f:
+    pickle.dump(res_dict, f)
+
+# with open('saved_dictionary.pkl', 'rb') as f:
+#     loaded_dict = pickle.load(f)
